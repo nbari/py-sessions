aaaa550c82ef9b8c6c48d0e1198824d21ec18b47
nbari <nbari@dalmp.com>
2014-07-30 15:59:37 +0000

    modified:   sessions/backends/gae_datastore_handler.py

---
+++ sessions/backends/gae_datastore_handler.py
@@ -0,0 +1,48 @@
+import uuid
+
+from . import HandlerBase
+from datetime import datetime, timedelta
+from google.appengine.ext import ndb
+
+
+class pySessions(ndb.Model):
+    # sid is the key/id
+    data = ndb.BlobProperty(required=True)
+    expiry = ndb.DateTimeProperty(required=True)
+
+    @classmethod
+    def get_sid(cls, sid):
+        sid_key = ndb.Key('pySessions', sid)
+        q = pySessions.query(
+            pySessions.key == sid_key,
+            pySessions.expiry >= datetime.utcnow())
+        return q.get()
+
+    @classmethod
+    def del_sid(cls, sid):
+        sid_key = ndb.Key('pySessions', sid)
+        return sid_key.delete()
+
+
+class Handler(HandlerBase):
+
+    def set(self, sid, data, ttl=0):
+        expire = datetime.utcnow() + timedelta(seconds=ttl)
+        try:
+            return pySessions(
+                id='%s' % sid,
+                data=data,
+                expiry=expire).put().id()
+        except Exception:
+            return
+
+    def get(self, sid):
+        session = pySessions.get_sid(sid)
+        if session:
+            return session.data
+
+    def delete(self, sid):
+        return pySessions.del_sid(sid)
+
+    def make_sid(self):
+        return uuid.uuid4().hex
